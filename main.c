88d473a6763468395e1cb23bb3cc0fa56150a741
Pierre-Martinant <pierremartinant60@gmail.com>
2025-03-05 14:56:57 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -2,17 +2,19 @@
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 #include "piece.h"
 #include "board.h"
 
 void knight_moves ();
-void game ();
+char players ();
 
 int main() {
     Piece **board = generateEmptyBoard();
     resetBoardPiece(board);
     displayBoard(board);
-    game();
+    players();
     deleteBoard(board);
     return 0;
 }
@@ -21,16 +23,13 @@ void knight_moves() {
 }
 
 
-void game() {
+char players() {
     char play1[50];
     char play2[50];
-
     printf("Player 1: ");
     scanf("%49s", play1);
-
     printf("Player 2: ");
     scanf("%49s", play2);
-
     printf("Joueur 1 : %s\n", play1);
     printf("Joueur 2 : %s\n", play2);
 }
