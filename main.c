8dcc82f1314da08ebb39156da91cedbab3e63321
Pierre-Martinant <pierremartinant60@gmail.com>
2025-03-06 11:19:36 +0100

    new commit

--- main.c
+++ main.c
@@ -7,29 +7,73 @@
 #include "piece.h"
 #include "board.h"
 
-void knight_moves ();
+char play1[50];
+char play2[50];
+char color1[5];
+char color2[5];
+
 char players ();
+int isDraw(Piece **board[8][8]);
+void deleteplayerinfo ();
 
 int main() {
     Piece **board = generateEmptyBoard();
-    resetBoardPiece(board);
+    //resetBoardPiece(board);
+    placePiece(board, E, 8, BLACK_KING);
+    placePiece(board, E, 1, WHITE_KING);
     displayBoard(board);
     players();
+    isDraw(board);
     deleteBoard(board);
+    deleteplayerinfo();
+    printf("it's show %s", play1);
     return 0;
-}
-
-void knight_moves() {
 }
 
 
 char players() {
-    char play1[50];
-    char play2[50];
     printf("Player 1: ");
     scanf("%49s", play1);
     printf("Player 2: ");
     scanf("%49s", play2);
-    printf("Joueur 1 : %s\n", play1);
-    printf("Joueur 2 : %s\n", play2);
+    printf("black or white: ");
+    scanf("%5s", color1);
+    if (color1 == "black") {
+        char color2 = "white";
+    }
+    else if (color1 == "white") {
+        char color2 = "black";
+    }
+    printf("%s is : %s\n", play1, color1);
+    printf("%s is : %s\n", play2, color2);
+}
+
+int isDraw(Piece **board[8][8]) {
+    int WKing = 0, BKing = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (board[i][j] = WHITE_KING) {
+                WKing = 1;
+            }
+            if (board[i][j] = BLACK_KING) {
+                BKing = 1;
+            }
+        }
+    }
+
+    // If one of the kings is missing, the game is a draw
+    if ("R" == 0 || "r" == 0) {
+        printf("One of the King is missing\n");
+        return 1;  // Draw
+    }
+
+    return 0;  // The game continues
+}
+
+void deleteplayerinfo () {
+    // Free all row before board
+    free(play1);
+    free(play2);
+    free(color1);
+    free(color2);
 }
