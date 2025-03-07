c16e4eba2041436ec6a6963b32bed0290f831fe1
hcavy6 <hugo.cavy@hesias.fr>
2025-03-07 14:19:46 +0100

    Echec Et Mat

--- Project/EchecetMat.c
+++ Project/EchecetMat.c
@@ -2,18 +2,14 @@
 // Created by cavyh on 07/03/2025.
 //
 
-#include <stdbool.h>
-
-#define SIZE 8
-
-bool estEchecEtMat(Piece board[SIZE][SIZE], char kingColor) {
+bool checkmate(Piece board, char kingColor) {
     // Vérifier si le roi est en échec
-    if (!estEnEchec(board, kingColor)) {
+    if (!ischeck(board, kingColor)) {
         return false; // Pas d'échec, donc pas d'échec et mat
     }
 
     // Vérifier s'il existe un mouvement légal pour sortir de l'échec
-    if (mouvementLegalPossible(board, kingColor)) {
+    if (legalmoovpossible(board, kingColor)) {
         return false; // Un mouvement légal existe, donc pas d'échec et mat
     }
 
